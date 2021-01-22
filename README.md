9d120a18a5f00698a9c68f901e2c5d17e6664009
Marek Laska <git@tamcap.one.pl>
2021-01-22 09:41:32 -0500

    New new CDC-LTC data. Also, some minor data points from CDC duplicate runs

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 Data has been collected from the official websites presenting COVID-19 vaccine progress.
 
 * [Centers for Disease Control and Prevention](https://covid.cdc.gov/covid-data-tracker/#vaccinations) is updated every workday, excluding federal holidays. If update falls on a federal holiday, there will be an update the next day.
+* [Centers for Disease Control and Prevention LTC](https://covid.cdc.gov/covid-data-tracker/#vaccinations-ltc) is updated as above. First data point is 2021-01-21.
 * [NC Department of Health and Human Services](https://covid19.ncdhhs.gov/dashboard/vaccinations) is updated every Tuesday, and other days if they feel like it.
 
 No warranties are made as to the accuracy of this data. Ultimately, I am just just a stranger on the Internet - this data should not be used to inform clinical or public health practice without verification via secondary sources.
--- tools/get_new.sh
+++ tools/get_new.sh
@@ -3,20 +3,25 @@
 source .env
 
 curl --silent $CDC_DATA_ENDPOINT -o /tmp/current.json
+curl --silent $CDC_DATA_LTC_ENDPOINT -o /tmp/current-ltc.json
 
 FILE_SUFFIX='_cdc_data'
+LTC_FILE_SUFFIX="_cdc_ltc_data"
 
 # what's the new run date?
 DATA_DATE=$(cat /tmp/current.json | jq ".vaccination_data[0].Date" | sed  's/[-"]//g')
+LTC_DATA_DATE=$(cat /tmp/current-ltc.json | jq ".vaccination_ltc_data[0].Date" | sed  's/[-"]//g')
 # echo $DATA_DATE
 
 # what's the new run id?
 DATA_RUNID=$(cat /tmp/current.json | jq ".runid")
+LTC_DATA_RUNID=$(cat /tmp/current-ltc.json | jq ".runid")
 # echo $DATA_RUNID
 
 # existing run id list
 DATA_CURRENT_RUNS=$(jq ".runid" ./data/*${FILE_SUFFIX}.json)
-# echo $DATA_CURRENT_RUNS
+LTC_DATA_CURRENT_RUNS=$(jq ".runid" ./data/*${LTC_FILE_SUFFIX}.json)
+echo $LTC_DATA_CURRENT_RUNS
 
 if [[ ${DATA_CURRENT_RUNS[*]} =~ $DATA_RUNID ]]
 then
@@ -25,4 +30,13 @@ then
 else
     echo "Run $DATA_RUNID ($DATA_DATE) is new, creating ./data/${DATA_DATE}_${DATA_RUNID}${FILE_SUFFIX}.json"
     mv /tmp/current.json ./data/${DATA_DATE}_${DATA_RUNID}${FILE_SUFFIX}.json
+fi
+
+if [[ ${LTC_DATA_CURRENT_RUNS[*]} =~ $LTC_DATA_RUNID ]]
+then
+    echo "LTC run $LTC_DATA_RUNID ($DATA_DATE) already exists, discarding"
+    rm -f /tmp/current-ltc.json
+else
+    echo "LTC run $LTCDATA_RUNID ($DATA_DATE) is new, creating ./data/${DATA_DATE}_${DATA_RUNID}${LTC_FILE_SUFFIX}.json"
+    mv /tmp/current-ltc.json ./data/${DATA_DATE}_${DATA_RUNID}${LTC_FILE_SUFFIX}.json
 fi
